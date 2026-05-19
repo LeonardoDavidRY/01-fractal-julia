18aee3d0279dd8db6239dd94cc1c57c746a233bf
LeonardoDavidRY <ldramirez@uce.edu.ec>
2026-05-19 08:37:20 -0500

    fractal con openmp

---
+++ fractal_openmp.cpp
@@ -0,0 +1,177 @@
+#include "fractal_openmp.h"
+#include "palette.h"
+#include <complex>
+#include <omp.h>
+#include <immintrin.h> 
+
+extern int max_iteraciones;
+extern std::complex<double> c;
+uint32_t acotado_openmp(double x, double y)
+{
+    /*
+    dados: c,zo
+    Zn+1 = Zn^2 + c
+    */
+    int iter = 1;
+
+    double zr = x;
+    double zi = y;
+
+    while (iter < max_iteraciones && (zr * zr + zi * zi) <= 4.0)
+    {
+
+        // Zn+1 = Zn^2 + c
+        double temp_zr = zr * zr - zi * zi + c.real();
+        double temp_zi = 2.0 * zr * zi + c.imag();
+
+        zr = temp_zr;
+        zi = temp_zi;
+
+        iter++;
+    }
+    if (iter < max_iteraciones)
+    {
+        // la norma > 2
+        int index = iter % PALETTE_SIZE;
+        return color_ramp[index];
+    }
+    return 0xFF000000;
+    //
+}
+void julia_openmp_regiones(double x_min, double x_max, double y_min, double y_max, uint32_t width, uint32_t height, uint32_t *pixel_buffer)
+{
+    double dx = (x_max - x_min) / width;
+    double dy = (y_max - y_min) / height;
+
+#pragma omp parallel
+    {
+        int thread_count = omp_get_num_threads();
+        int thread_id = omp_get_thread_num();
+
+        int delta = std::ceil(width * 1.0 / thread_count);
+
+        int start = thread_id * delta;
+        int end = (thread_id + 1) * delta; // end = start + delta
+
+        if (thread_id == thread_count - 1)
+        {
+            end = width; // el ultimo thread se encarga del resto
+        }
+        for (int i = start; i < end; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                // z = x+yi = (x,y)
+                double x = x_min + i * dx;
+                double y = y_max - j * dy;
+                auto color = acotado_openmp(x, y);
+                pixel_buffer[i + j * width] = color;
+            }
+        }
+    }
+}
+void julia_openmp_for(double x_min, double x_max, double y_min, double y_max, uint32_t width, uint32_t height, uint32_t *pixel_buffer){
+    double dx = (x_max - x_min) / width;
+    double dy = (y_max - y_min) / height;
+
+    #pragma omp parallel for
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            // z = x+yi = (x,y)
+            double x = x_min + i * dx;
+            double y = y_max - j * dy;
+            auto color = acotado_openmp(x, y);
+            pixel_buffer[i + j * width] = color;
+        }
+    }
+}
+void julia_openmp_for_simd(double x_min, double x_max, double y_min, double y_max, uint32_t width, uint32_t height, uint32_t *pixel_buffer){
+    double dx = (x_max - x_min) / width;
+    double dy = (y_max - y_min) / height;
+
+    // esto hara el siguiente vector:  x_min x_min x_min x_min x_min x_min x_min x_min
+    __m256 xmin = _mm256_set1_ps(x_min);
+    __m256 ymax = _mm256_set1_ps(y_max); // igual 8 copias de y_max
+    __m256 xscale = _mm256_set1_ps(dx);
+    __m256 yscale = _mm256_set1_ps(dy);
+    __m256 c_real = _mm256_set1_ps(c.real());
+    __m256 c_imag = _mm256_set1_ps(c.imag());
+
+    // se neceita la copia de las 4 normas para calcular cuando se menor a 4
+    __m256 threshold = _mm256_set1_ps(4.0f);
+
+    __m256 one = _mm256_set1_ps(1.0f);
+
+    #pragma omp parallel for
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j += 8)
+        {
+            // i es ancho y j alto estas igual se crea un vectro de 8 com de la j
+            __m256 mx = _mm256_set1_ps(i);
+            __m256 my = _mm256_set_ps(j + 7, j + 6, j + 5, j + 4, j + 3, j + 2, j + 1, j ); // 8 copias de j siempre se saca al reves y se debe tener cuidado de tomarlo de forma correcta para graficar el pixel
+
+            // los tipos de datos en con 2_ mientras que las operacion con 1_
+
+            // primero se multipla
+            __m256 cr = _mm256_add_ps(_mm256_mul_ps(mx, xscale), xmin); // i*dx + x_min
+
+            __m256 ci = _mm256_sub_ps(ymax, _mm256_mul_ps(my, yscale) ); // y_max - j*dy
+
+            int iter = 1;
+
+            __m256 zr = cr;
+            __m256 zi = ci;
+
+            __m256 mk = _mm256_set1_ps(iter); // contador de iteraciones para cada uno de los 8 puntos
+
+            while (iter < max_iteraciones)
+            {
+                // Zn+1 = Zn^2 + c
+                __m256 zr2 = _mm256_mul_ps(zr, zr);  // zr^2
+                __m256 zi2 = _mm256_mul_ps(zi, zi);  // zi^2
+                __m256 zrzi = _mm256_mul_ps(zr, zi); // zr*zi
+
+                zr = _mm256_add_ps(_mm256_sub_ps(zr2, zi2), c_real);      // zr^2 - zi^2 + cr
+                zi = _mm256_add_ps(_mm256_add_ps(zrzi, zrzi), c_imag);    // 2*zr*zi + ci
+                                                                          // ahora hallar la norma
+                zr2 = _mm256_mul_ps(zr, zr);                              // zr^2
+                zi2 = _mm256_mul_ps(zi, zi);                              // zi^2
+                __m256 mag2 = _mm256_add_ps(zr2, zi2);                    // norma = zr^2 + zi^2
+                                                                          // comparar las 8 normas
+                __m256 mask = _mm256_cmp_ps(mag2, threshold, _CMP_LE_OS); // mag2 < 4.0 seran 8 resultados, es decir que es la mascara
+
+                mk = _mm256_add_ps(_mm256_and_ps(mask, one), mk);
+
+                if (_mm256_movemask_ps(mask) == 0)
+                {
+                    // todos ya divergen
+                    break;
+                }
+                iter++;
+            }
+            float d[8];
+            _mm256_storeu_ps(d, mk); // guarda el resultado la cantidad de iteracion hizo cada uno
+
+            for (int it = 0; it < 8; it++)
+            {
+                int index = (j + it) * width + i;
+                if (index < width * height) // para segurar que no se va a salir de la imagen
+                {
+                    if (d[it] < max_iteraciones)
+                    {
+                        int color_idx = (int)d[it] % PALETTE_SIZE;
+                        auto color = color_ramp[color_idx];
+                        pixel_buffer[index] = color;
+                    }
+                    else
+                    {
+                        pixel_buffer[index] = 0xFF000000;
+                    }
+                }
+            }
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <fmt/core.h>
 #include "fractal_serial.h"
 #include "fractal_simd.h"
+#include "fractal_openmp.h"
+#include <omp.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -21,18 +23,28 @@ std::complex<double> c(-0.7, 0.27015);
 // Textura
 uint32_t *pixel_buffer = nullptr;
 
-enum class runtime_type {
-  SERIAL_1 = 0,
-  SERIAL_2,
-  SIMD
+enum class runtime_type
+{
+    SERIAL_1 = 0,
+    SERIAL_2,
+    SIMD,
+    OPENMP_REGIONES,
+    OPENMP_FOR,
+    OPENMP_FOR_SIMD
 };
 int main()
 {
+    int thread_count;
 
-    
+    #pragma omp parallel
+    {
+        #pragma omp master
+        thread_count = omp_get_num_threads();
+    }
+
     pixel_buffer = new uint32_t[WIDTH * HEIGHT];
 
-    runtime_type r_type = runtime_type::SERIAL_1; 
+    runtime_type r_type = runtime_type::SERIAL_1;
 
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGHT}), "Julia Set - SFML");
@@ -50,14 +62,13 @@ int main()
     text.setPosition({10, 10});
     text.setStyle(sf::Text::Bold);
 
-    std::string options = "Options [1] Serial 1 [2] Serial 2, [3] SIMD  | UP/DOWN: Change Iterations";
-    sf::Text textOptions(font, options, 24);;
+    std::string options = "Options [1] Serial 1 [2] Serial 2, [3] SIMD, [4] OPENMP_REGIONES, [5] OPENMP_FOR, [6] OPENMP_FOR_SIMD | UP/DOWN: Change Iterations";
+    sf::Text textOptions(font, options, 24);
+    ;
     textOptions.setFillColor(sf::Color::White);
     textOptions.setStyle(sf::Text::Bold);
-    textOptions.setPosition({10, window.getView().getSize().y -40});
+    textOptions.setPosition({10, window.getView().getSize().y - 40});
 
-    
-  
     int frames = 0;
     int fps = 0;
     sf::Clock clock;
@@ -92,9 +103,20 @@ int main()
                 case sf::Keyboard::Scan::Numpad3:
                     r_type = runtime_type::SIMD;
                     break;
+                case sf::Keyboard::Scan::Num4:
+                case sf::Keyboard::Scan::Numpad4:
+                    r_type = runtime_type::OPENMP_REGIONES;
+                    break;
+                case sf::Keyboard::Scan::Num5:
+                case sf::Keyboard::Scan::Numpad5:
+                    r_type = runtime_type::OPENMP_FOR;
+                    break;
+                case sf::Keyboard::Scan::Num6:
+                case sf::Keyboard::Scan::Numpad6:
+                    r_type = runtime_type::OPENMP_FOR_SIMD;
+                    break;
                 }
-                
-                  
+                std::memset(pixel_buffer, 0, WIDTH * HEIGHT * sizeof(uint32_t));
             }
         }
         // dibujar:
@@ -103,22 +125,35 @@ int main()
         if (r_type == runtime_type::SERIAL_1)
         {
             julia_serial1(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
-            mode= "SERIAL_1";
+            mode = "SERIAL_1";
         }
         else if (r_type == runtime_type::SERIAL_2)
         {
-            
+
             julia_serial2(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
-            mode= "SERIAL_2";
+            mode = "SERIAL_2";
         }
         else if (r_type == runtime_type::SIMD)
         {
-            
-            julia_simd(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
-            mode= "SIMD";
-        }
-        
 
+            julia_simd(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
+            mode = "SIMD";
+        }
+        else if (r_type == runtime_type::OPENMP_REGIONES)
+        {
+            julia_openmp_regiones(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
+            mode = fmt::format("OPENMP_REGIONES ({} threads)", thread_count);
+        }
+        else if (r_type == runtime_type::OPENMP_FOR)
+        {
+            julia_openmp_for(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
+            mode = fmt::format("OPENMP_FOR ({} threads)", thread_count);
+        }
+        else if (r_type == runtime_type::OPENMP_FOR_SIMD)
+        {
+            julia_openmp_for_simd(x_min, x_max, y_min, y_max, WIDTH, HEIGHT, pixel_buffer);
+            mode = fmt::format("OPENMP_FOR_SIMD ({} threads)", thread_count);
+        }
         texture.update((const uint8_t *)pixel_buffer);
 
         // contar FPS
